5f106927ca052767d627842a331cdd4055264379
ClemMarkey <markey.clement@gmail.com>
2019-06-17 14:19:14 +0200

    Coucou

---
+++ app.js
@@ -0,0 +1,34 @@
+/*
+! Consigne: développer un jeu en JavaScript. Le <body> doit être vide !
+! La V1 doit être push sur GitHub avant le 28 Juin Minuit !
+
+
+*   Règles du jeu:
+*   Le joueur doit cliquer sur des formes pour les éliminer dans un temps imparti
+*   Chaque élimination rapporte 1 point au joueur
+*   Les objets de couleur noire sont les ennemis. Si on en élimine, la partie est terminée
+*   Si tous les autres objets sont éliminés il reste donc uniquement les noirs. La partie est gagnée !
+
+?   Le jeu contient 3 niveaux de difficulté dont dépend le nombre de formes
+*   Noob = Objets: 20 dont 3 noirs| Temps: 60 secondes
+*   Hardcore = Objets: 80 dont 20 noirs | Temps: 45 secondes
+*   Ultraviolence = Objets: 200 dont 70 noirs | Temps: 30 secondes
+
+*   Conditions:
+*   Tout le code doit être généré via le JS !
+*   Les objets ne peuvent pas être des images, ils sont full code !
+*   A chaque objet éliminé, les autres restants changent de position
+*   Si le temps imparti est terminé et que tous les objetsne sont pas éliminés: Afficher score
+*   Si la partie est gagnée (Touts les formes ont été eliminées dans le temps imparti): Afficher le temps
+
+*/
+
+/*
+
+Pseudo code:
+
+Créer une div container
+Créer les formes en CSS
+
+
+*/
